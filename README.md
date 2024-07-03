c1e0fd70ec5556bf8c209a3f75f52d80625e68e4
Jiempy <contacto@jiempy.cl>
2024-07-03 16:04:41 -0400

    Se incorpora la creacion de .min.css y css.map

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # INTRUCCIONES DE USO en vsCode
 
-## Verifica si tienes instalado uglify-js
-- Para instalar ejecutar "npm install uglify-js -g"
+## Verifica si tienes instalado uglify-js y clean-css-cli
+- Para instalar ejecutar "npm install -g clean-css-cli uglify-js"
 
-## Codigo a ejecutar para crear los .map
+## Codigo a ejecutar para crear taks en .vscode los .map
+- Agregar al directorio files todos los .css y .js que deseas obtener los .min y/o los .map 
 - Para ejecutar las Tasks usar comando: "node generate-tasks.js"
+
+## Ejecutar Tasks para crear los .min y los .map correspondientes
 - Presiona "Ctrl+Shift+P" para abrir la paleta de comandos de VS Code.
 - Escribe "Tasks: Run Task" y selecciona esa opción.
 - Luego selecciona la tarea minify-all que configuraste.
+
+## Output
+- Se crearan 2 carpetas (files/css y files/js) automaticamente donde se dejaran los archivos .min y .map
 
--- generate-tasks.js
+++ generate-tasks.js
@@ -1,25 +1,124 @@
 const fs = require("fs");
 const path = require("path");
+const os = require("os");
 
-const directory = "files"; // Directorio donde están los archivos JavaScript
-const files = fs.readdirSync(directory).filter((file) => file.endsWith(".js"));
+const directory = "files"; // Directorio donde están los archivos CSS y JavaScript
+const jsOutputDirectory = path.join(directory, "js");
+const cssOutputDirectory = path.join(directory, "css");
 
-const tasks = files.map((file) => {
+// Crear directorios de salida si no existen
+if (!fs.existsSync(jsOutputDirectory)) {
+  fs.mkdirSync(jsOutputDirectory);
+}
+
+if (!fs.existsSync(cssOutputDirectory)) {
+  fs.mkdirSync(cssOutputDirectory);
+}
+
+// TRABAJANDO CON LOS .JS
+
+const uglifyjsExecutable = path.join(
+  os.homedir(),
+  "AppData",
+  "Roaming",
+  "npm",
+  "uglifyjs"
+);
+
+const jsFiles = fs
+  .readdirSync(directory)
+  .filter((file) => file.endsWith(".js"));
+
+const jsTasks = jsFiles.map((file) => {
   const filePath = path.posix.join(directory, file);
   const minifiedFile = path.posix.join(
-    directory,
-    path.parse(file).name + ".js"
+    jsOutputDirectory,
+    path.parse(file).name.replace(".min", "") + ".min.js"
   );
-  const sourceMapFile = minifiedFile + ".map";
+  const sourceMapFile = `${minifiedFile}.map`; // Archivo de mapa
 
-  return {
-    label: `minify-${file}`,
-    type: "shell",
-    command: `uglifyjs ${filePath} --source-map "filename='${sourceMapFile}'" -o "${minifiedFile}"`,
-    group: "none",
-    problemMatcher: [],
-  };
+  // Verificar si ya existe el archivo .min.js
+  const jsMinExists = fs.existsSync(minifiedFile);
+
+  if (jsMinExists) {
+    // Crear tarea solo para generar el archivo .map
+    return {
+      label: `generate-map-js-${file}`,
+      type: "shell",
+      command: `& "${uglifyjsExecutable}" "${filePath}" --source-map "filename='${sourceMapFile.replace("\\","/")}'" -o "${minifiedFile.replace("\\","/")}"`,
+
+      group: "none",
+      problemMatcher: [],
+      presentation: {
+        reveal: "always",
+        panel: "shared",
+        clear: false,
+      },
+    };
+  } else {
+    // Crear tarea para minificar y generar el archivo .map
+    return {
+      label: `minify-js-${file}`,
+      type: "shell",
+      command: `& "${uglifyjsExecutable}" "${filePath}" --source-map "filename='${sourceMapFile.replace("\\","/")}'" -o "${minifiedFile.replace("\\","/")}"`,
+      group: "none",
+      problemMatcher: [],
+      presentation: {
+        reveal: "always",
+        panel: "shared",
+        clear: false,
+      },
+    };
+  }
 });
+
+//TRABAJANDO CON LOS .CSS
+
+const cleancssExecutable = path.join(
+  os.homedir(),
+  "AppData",
+  "Roaming",
+  "npm",
+  "cleancss"
+);
+
+const cssFiles = fs
+  .readdirSync(directory)
+  .filter((file) => file.endsWith(".css"));
+
+const cssTasks = cssFiles.map((file) => {
+  const filePath = path.posix.join(directory, file);
+  const minifiedFile = path.posix.join(
+    cssOutputDirectory,
+    path.parse(file).name.replace(".min", "") + ".min.css"
+  );
+  const sourceMapFile = `${minifiedFile}.map`; // Archivo de mapa
+
+  // Verificar si ya existe el archivo .min.css
+  const cssMinExists = fs.existsSync(minifiedFile);
+
+  if (cssMinExists) {
+    // Crear tarea solo para generar el archivo .map
+    return {
+      label: `generate-map-css-${file}`,
+      type: "shell",
+      command: `& "${cleancssExecutable}" ${filePath} --source-map --source-map-inline-sources`,
+      group: "none",
+      problemMatcher: [],
+    };
+  } else {
+    // Crear tarea para minificar y generar el archivo .map
+    return {
+      label: `minify-css-${file}`,
+      type: "shell",
+      command: `& "${cleancssExecutable}" ${filePath} -o "${minifiedFile}" --source-map --source-map-inline-sources`,
+      group: "none",
+      problemMatcher: [],
+    };
+  }
+});
+
+const tasks = [...jsTasks, ...cssTasks];
 
 // Añadir tarea compuesta que ejecute todas las tareas individuales
 const compoundTask = {
@@ -44,4 +143,3 @@ if (!fs.existsSync(".vscode")) {
 fs.writeFileSync(".vscode/tasks.json", JSON.stringify(tasksJson, null, 2));
 
 console.log("Tareas generadas exitosamente en tasks.json.");
-
